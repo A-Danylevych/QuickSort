6c2feee6c9e8d87129806c7ff21ce7a9ef134e0b
anton <anton231255@gmail.com>
2022-03-24 12:03:56 +0200

    second generation algorithm

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -5,9 +5,20 @@ import (
 	"time"
 )
 
-func GenerateArray(size int) []int {
+func GeneratePermArray(size int) []int {
 	rand.Seed(time.Now().Unix())
 
 	var array = rand.Perm(size)
+	return array
+}
+
+func GenerateRandomArray(size, elementRange int) []int {
+	rand.Seed(time.Now().Unix())
+
+	array := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		array[i] = rand.Intn(elementRange) - rand.Intn(elementRange)
+	}
 	return array
 }
