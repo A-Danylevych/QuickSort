b136ac36949db9ff00cd7a70d568180caa6d8d71
anton <anton231255@gmail.com>
2022-03-24 11:45:20 +0200

    linear algorithm

---
+++ internal/quickSort/linearSorting.go
@@ -0,0 +1,28 @@
+package quickSort
+
+func LinearSort(array []int) []int {
+	return linearSort(array, 0, len(array)-1)
+}
+
+func linearSort(array []int, lowIndex, highIndex int) []int {
+	if lowIndex < highIndex {
+		var pivot int
+		array, pivot = partition(array, lowIndex, highIndex)
+		array = linearSort(array, lowIndex, pivot-1)
+		array = linearSort(array, pivot+1, highIndex)
+	}
+	return array
+}
+
+func partition(array []int, lowIndex, highIndex int) ([]int, int) {
+	pivot := array[highIndex]
+	i := lowIndex
+	for j := lowIndex; j < highIndex; j++ {
+		if array[j] < pivot {
+			array[i], array[j] = array[j], array[i]
+			i++
+		}
+	}
+	array[i], array[highIndex] = array[highIndex], array[i]
+	return array, i
+}
